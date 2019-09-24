90f0235fe392696c1c19a4e5266ef995254d043f
Saima H <saima_h18@hotmail.com>
2019-09-24 18:42:02 -0400

    Updated MX,NS

--- lab1_416/src/lab1_416/DnsClient.java
+++ lab1_416/src/lab1_416/DnsClient.java
@@ -23,214 +23,214 @@ public class DnsClient {
 
 	public static void main(String args[]) throws Exception
 	{
-		
-		
+
+
 		int timeout = 5;
 		int maxRetry = 3;
 		int port = 53;
-	    String Qtype = "A"; //1 = A , 2= NS, 15= MX
-	    
-	    String Qname = "";
-	    String ip = "";
-	    //String ipArr[] = new String [4];
-	    //String ipArr[] = {"","","","",};
+		String Qtype = ""; //1 = A , 2= NS, 15= MX
 
-	    int indxArgs = 0;
-	    boolean at = false;
-	    //reading the args
-		
-		
+		String Qname = "";
+		String ip = "";
+		//String ipArr[] = new String [4];
+		//String ipArr[] = {"","","","",};
+
+		int indxArgs = 0;
+		boolean at = false;
+		//reading the args
+
+
 		try {
 
-		      for (int i = 0; i < args.length; i++) {
-		        char[] arrArgs = args[i].toCharArray();            
-		        for (int j = 0; j < arrArgs.length; j++) {                     
-		          if (arrArgs[j] == '@' ) {
-		            
-		            ip = args[i];
-		            Qname = args[i+1]; 
-		            at = true;
-		          }                   
-		          else if (arrArgs[j] == '.') 
-		            indxArgs++ ;
+			for (int i = 0; i < args.length; i++) {
+				char[] arrArgs = args[i].toCharArray();            
+				for (int j = 0; j < arrArgs.length; j++) {                     
+					if (arrArgs[j] == '@' ) {
 
-		          else if (arrArgs[j] != '@' && Character.isDigit(arrArgs[j]) && at)
-		            //ipArr[indxArgs] += arrArgs[j];
-		            ip = args[i];
-		            //System.out.println("args" + args[i]);
-		            
+						ip = args[i];
+						Qname = args[i+1]; 
+						at = true;
+					}                   
+					else if (arrArgs[j] == '.') 
+						indxArgs++ ;
 
-		          else if (arrArgs[j] == '-'){
-		            if (arrArgs[j+1] == 't') {
-		              timeout = Integer.parseInt(args[i+1]);
-		            }
-		            if (arrArgs[j+1] == 'r') {
-		              maxRetry = Integer.parseInt(args[i+1]);
-		            }
-		            if (arrArgs[j+1] == 'p') {
-
-		              port = Integer.parseInt(args[i+1]);
-		            }
-
-		            if (arrArgs[j+1] == 'm' && arrArgs[j+2] == 'x')
-		              Qtype = "MX";
-
-		            if (arrArgs[j+1] == 'n' && arrArgs[j+2] == 's')
-		              Qtype = "NS";
-		          }
-		        }   
-		      }
-
-		      if (!at) {                              
-		        System.out.println("The IP address entered is not correct. Please ensure to enter'@'");
-		        System.exit(1);
-		      }
+					else if (arrArgs[j] != '@' && Character.isDigit(arrArgs[j]) && at)
+						//ipArr[indxArgs] += arrArgs[j];
+						ip = args[i];
+					//System.out.println("args" + args[i]);
 
 
-		      //            if (atSign >1) {
-		      //                System.out.println("ERROR   Only enter one '@'");
-		      //                System.exit(1);
-		      //            }
+					else if (arrArgs[j] == '-'){
+						if (arrArgs[j+1] == 't') {
+							timeout = Integer.parseInt(args[i+1]);
+						}
+						if (arrArgs[j+1] == 'r') {
+							maxRetry = Integer.parseInt(args[i+1]);
+						}
+						if (arrArgs[j+1] == 'p') {
 
-		      if (timeout == 0){
-		        System.out.println("Please enter a value greater than 0 for the timeout");
-		        System.exit(1);
-		      }
+							port = Integer.parseInt(args[i+1]);
+						}
 
-		    } 
+						if (arrArgs[j+1] == 'm' && arrArgs[j+2] == 'x')
+							Qtype = "MX";
+
+						if (arrArgs[j+1] == 'n' && arrArgs[j+2] == 's')
+							Qtype = "NS";
+					}
+				}   
+			}
+
+			if (!at) {                              
+				System.out.println("The IP address entered is not correct. Please ensure to enter'@'");
+				System.exit(1);
+			}
 
 
-		    catch (IllegalArgumentException e) {
-		      System.out.println("Please make sure your arguments follows the correct format");
-		      System.exit(1);
-		    }
-		    if (ip == null || Qname == null) {
-		      System.out.println("Please make sure to provide the IP address and domain name");
-		      System.exit(1);
-		    }
-		    
-		    
-				
+			//            if (atSign >1) {
+			//                System.out.println("ERROR   Only enter one '@'");
+			//                System.exit(1);
+			//            }
+
+			if (timeout == 0){
+				System.out.println("Please enter a value greater than 0 for the timeout");
+				System.exit(1);
+			}
+
+		} 
+
+
+		catch (IllegalArgumentException e) {
+			System.out.println("Please make sure your arguments follows the correct format");
+			System.exit(1);
+		}
+		if (ip == null || Qname == null) {
+			System.out.println("Please make sure to provide the IP address and domain name");
+			System.exit(1);
+		}
+
+
+
 		// Create a UDP socket
 		// (Note, when no port number is specified, the OS will assign an arbitrary one)
 		DatagramSocket clientSocket = new DatagramSocket();
-		
+
 		// Resolve a domain name to an IP address object
 		// In this case, "localhost" maps to the so-called loop-back address, 127.0.0.1
-		
-	    ip= ip.replaceAll("@","");
-		String [] bytesString= ip.split("\\.");
-			    
 
-//		   System.out.print(bytesString.length + "\n");
-//		   for(int i=0; i< bytesString.length ; i++) {
-//		     System.out.print(bytesString[i] +" ");
-//		  } 
-		   
-		   
-		   // why is 132 being printed as -124????
-		   byte[] IPBytes = new byte [] {(byte) (Integer.parseInt(bytesString[0])), (byte) (Integer.parseInt(bytesString[1])), (byte) (Integer.parseInt(bytesString[2])), (byte) (Integer.parseInt(bytesString[3]))};
-		    
-		   //byte[] IPAddress = new byte [] {(byte) (IPint[0]), (byte) (IPint[1]), (byte) (IPint[2]), (byte) (IPint[3])};
-		  // IPAddress[0] = (byte) unByte(IPAddress[0]);
-		   
-		   
-//		   for(int i=0; i< IPBytes.length ; i++) {
-//		      System.out.print(IPBytes[i] +" ");
-//		   }
-		      
-		      
+		ip= ip.replaceAll("@","");
+		String [] bytesString= ip.split("\\.");
+
+
+		//		   System.out.print(bytesString.length + "\n");
+		//		   for(int i=0; i< bytesString.length ; i++) {
+		//		     System.out.print(bytesString[i] +" ");
+		//		  } 
+
+
+		// why is 132 being printed as -124????
+		byte[] IPBytes = new byte [] {(byte) (Integer.parseInt(bytesString[0])), (byte) (Integer.parseInt(bytesString[1])), (byte) (Integer.parseInt(bytesString[2])), (byte) (Integer.parseInt(bytesString[3]))};
+
+		//byte[] IPAddress = new byte [] {(byte) (IPint[0]), (byte) (IPint[1]), (byte) (IPint[2]), (byte) (IPint[3])};
+		// IPAddress[0] = (byte) unByte(IPAddress[0]);
+
+
+		//		   for(int i=0; i< IPBytes.length ; i++) {
+		//		      System.out.print(IPBytes[i] +" ");
+		//		   }
+
+
 		InetAddress ipAddress = InetAddress.getByAddress(IPBytes);
-		
-		
+
+
 		// Allocate buffers for the data to be sent and received
 		byte[] sendData = new byte[1024];		
 		byte[] receiveData = new byte[1024];
-		
 
-		
+
+
 		Random y = new Random(); 
 		byte[] two = new byte[2];
 		y.nextBytes(two);
-		
+
 		// Header
 		sendData[0] = two[0];
 		sendData[1] = two[1];
-		
-		sendData[2] = 00000001;  //QR =0 , PCODE = 0000 , AA =0 , TC=0 , RD =1 Recursion
-		
-		sendData[3] = 00000000; // Ra, Z, RCODE = 0
-		
-		sendData[4] = 00000000; // QDCOUNT = 1 , 16 bits
-		
-	    sendData[5] = 00000001; // QDCOUNT = 1 , 16 bits
-		
-        sendData[6] = 00000000; // 
-		
-	    sendData[7] = 00000000; // 
-	    
-	    sendData[8] = 00000000; // 
-	    
-	    sendData[9] = 00000000; // 
-	    
-	    sendData[10] = 0; // 
-	    sendData[11] = 0; // 
-	    
-	    //DATA
-	    
-	    
-	    int indx = 12;
-	    
-	 String [] Qstrings= Qname.split("\\.");
-		 
-		 int[] len = new int[Qstrings.length];
-		 
-		 for(int i=0; i< Qstrings.length ; i++) {
-			  //System.out.print("len" +  Qstrings[i].length() + " ") ;
-			  sendData[indx++] = (byte) Qstrings[i].length();
-			  
-			  char[] carray = Qstrings[i].toCharArray();
-			  
-			  for(int j=0; j<carray.length; j++) {
-				 
-				  sendData[indx++] = (byte) carray[j]; //did operation then increment
-				  //System.out.print( "carray" + (byte) carray[j] + " ") ;
-				  
-			  }
-			 
-		   }
-		 //String pointer = Qstrings[Qstrings.length-1];
-		 
-		 //System.out.print( "index" + indx + "\n") ;
-		 //System.out.print(sendData[25] ) ;
-		 
-		 sendData[26] =0; // End QName field 
-		 
-		 
-		 if(Qtype == "A") {
-			 sendData[27] = 0;	
-			 sendData[28] = 1;	 
-		 }
-		 else if (Qtype == "NS") {
-				 sendData[27] = 0;	
-				 sendData[28] = 2;	
-		 }
-		 
-		 else if (Qtype == "MX") {
-			 sendData[27] = 0;	
-			 sendData[28] = 15;	
-	 }
-		 
-		 sendData[29] = 0;	
-		 sendData[30] = 1;	  // Qclass = 1
-			 		 
 
-		
+		sendData[2] = 00000001;  //QR =0 , PCODE = 0000 , AA =0 , TC=0 , RD =1 Recursion
+
+		sendData[3] = 00000000; // Ra, Z, RCODE = 0
+
+		sendData[4] = 00000000; // QDCOUNT = 1 , 16 bits
+
+		sendData[5] = 00000001; // QDCOUNT = 1 , 16 bits
+
+		sendData[6] = 00000000; // 
+
+		sendData[7] = 00000000; // 
+
+		sendData[8] = 00000000; // 
+
+		sendData[9] = 00000000; // 
+
+		sendData[10] = 0; // 
+		sendData[11] = 0; // 
+
+		//DATA
+
+
+		int indx = 12;
+
+		String [] Qstrings= Qname.split("\\.");
+
+		int[] len = new int[Qstrings.length];
+
+		for(int i=0; i< Qstrings.length ; i++) {
+			//System.out.print("len" +  Qstrings[i].length() + " ") ;
+			sendData[indx++] = (byte) Qstrings[i].length();
+
+			char[] carray = Qstrings[i].toCharArray();
+
+			for(int j=0; j<carray.length; j++) {
+
+				sendData[indx++] = (byte) carray[j]; //did operation then increment
+				//System.out.print( "carray" + (byte) carray[j] + " ") ;
+
+			}
+
+		}
+		//indx++;
+		//String pointer = Qstrings[Qstrings.length-1];
+
+		//System.out.print( "index" + indx + "\n") ;
+		//System.out.print(sendData[25] ) ;
+		sendData[indx++] =0; // End QName field 
+
+
+		if(Qtype.equals("A")) {
+			sendData[indx++] = 0;	
+			sendData[indx++] = 1;	 
+		}
+		else if (Qtype.equals("NS")) {
+			sendData[indx++] = 0;	
+			sendData[indx++] = 2;
+		}
+
+		else if (Qtype.equals("MX")) {
+			sendData[indx++] = 0;	
+			sendData[indx++] = 15;	
+		}
+
+		sendData[indx++] = 0;	
+		sendData[indx++] = 1;	  // Qclass = 1
+
+
+
 		// Create a UDP packet to be sent to the server
 		// This involves specifying the sender's address and port number
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
-		
-		
+
+
 		// Send the packet
 		clientSocket.send(sendPacket);
 		long startTime = System.currentTimeMillis();
@@ -240,13 +240,13 @@ public class DnsClient {
 		System.out.println("Server:"+ "[" + ip + "]");
 		System.out.println("Request type:" + "[" + Qtype + "]");
 
-		
+
 		// Create a packet structure to store data sent back by the server
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		
+
 		int retry = 0;
 		clientSocket.setSoTimeout(timeout*1000);
-		
+
 		while(retry<=maxRetry) {
 			try {
 				clientSocket.receive(receivePacket);
@@ -257,8 +257,8 @@ public class DnsClient {
 				retry++;
 			}
 		}
-		
-		
+
+
 		if(retry>maxRetry) {
 			try {
 				maxRetriesError(maxRetry);
@@ -267,49 +267,52 @@ public class DnsClient {
 				System.out.println("\n" + "ERROR" +"\t"+ "Maximum number of retries [" + maxRetry +"] exceeded");
 			}
 		}
-		
+
 		// Receive data from the server
-		
+
 		//clientSocket.receive(receivePacket);
 		long endTime = System.currentTimeMillis();
 		System.out.println();
 		long timeTaken = (long)((endTime - startTime));
 		System.out.println("Response received after " + timeTaken + " milliseconds " + "(["  + retry + "]) " + "retries");
-		
-		
+
+
 		// Extract the sentence (as a String object) from the received byte stream
 		String modifiedSentence = new String(receivePacket.getData());
 
-		
+
 		System.out.println("From Server: " + modifiedSentence);
 		
+		
+		/////reading response//////
+
 		byte[] byteRec = receivePacket.getData();
 
 		int dPointer = 12; //pointer before domain name begins
-		
+
 		while(byteRec[dPointer] != 0) {
 			dPointer++;		//domain name ends
 		}
-		
+
 		dPointer = dPointer+7; // add 8 bytes to signify the beginning of the Type response
 
 		dPointer = dPointer+1; // add 1 byte to skip over the byte of zeroes in the Type response
 
 		System.out.println("th"+dPointer);
 		String responseType= "";
-		
+
 		if(byteRec[dPointer]== 1) {
 			responseType = "A";
 		}
-		
+
 		if(byteRec[dPointer]== 2) {
 			responseType = "NS";
 		}
-		
+
 		if(byteRec[dPointer]== 15) {
 			responseType = "MX";
 		}
-		
+
 		if(byteRec[dPointer]== 5) {
 			responseType = "CNAME";
 		}
@@ -328,11 +331,11 @@ public class DnsClient {
 		dPointer++;
 		byte[] ttlArr = new byte[4];
 		int mask = 0xFF;
-		
+
 		ttlArr[0] = byteRec[dPointer];
 		System.out.println(dPointer);
 		dPointer=dPointer+1; // increment from index 37 to index 38..why?
-		
+
 		System.out.println("here");
 		int unSignedval1 = ttlArr[0] & mask;
 		ttlArr[1] = byteRec[dPointer++];
@@ -349,19 +352,19 @@ public class DnsClient {
 		System.out.println(unSignedval4);
 
 		System.out.println(Arrays.toString(ttlArr));
-		
+
 		String ttlString = Integer.toHexString(unSignedval1)+Integer.toHexString(unSignedval2)+Integer.toHexString(unSignedval3)+Integer.toHexString(unSignedval4);
 		System.out.println("ttl:");
 		int ttl = Integer.parseInt(ttlString,16);
 		System.out.println(ttlString); //hex
 		System.out.println(ttl);		//decimal
-		
-		
-		
+
+
+
 		byte[] rdArr = new byte[2];
 		//dPointer++; 
 		rdArr[0] = byteRec[dPointer++];
-		
+
 		int unSignedval5 = rdArr[0] & mask;
 		rdArr[1] = byteRec[dPointer++];
 		int unSignedval6 = rdArr[1] & mask;
@@ -369,8 +372,8 @@ public class DnsClient {
 		String rdString = Integer.toHexString(unSignedval5)+Integer.toHexString(unSignedval6);
 
 		System.out.println("rdString: "+rdString);
-		
-		
+
+
 		if(responseType == "A") {
 			byte[] IPArr = new byte[4];
 			//dPointer++; 
@@ -383,24 +386,18 @@ public class DnsClient {
 			IPArr[3] = byteRec[dPointer++];
 			int unSignedval10 = IPArr[3] & mask;
 			String IPString = Integer.toString(unSignedval7)+"."+Integer.toString(unSignedval8)+"."+Integer.toString(unSignedval9)+"."+Integer.toString(unSignedval10);
-	
+
 			System.out.println("IP \t" + IPString + "\t" + ttl + "\t" +"addauth");
 		}
-		
-		
+
+
 		else if (responseType == "NS") {
-			
-			System.out.println(Qname);
-			//System.out.println("NS \t" + alias + "\t" + ttl + "\t" + auth);
-		}
-		
-		else if (responseType == "CNAME") {
-			//dPointer++;
+
 			String alias = "";
 
 			while(byteRec[dPointer]!=0) {
 
-				
+
 				if((byteRec[dPointer] & 0xFF) >= 0xC0) {
 
 					dPointer++;
@@ -412,20 +409,17 @@ public class DnsClient {
 							pointer++;
 							alias = alias + (char)byteRec[pointer];
 						}
-					
+
 						dPointer++;
 						if(byteRec[dPointer]!=0) {
 							alias = alias + ".";
 							pointer++;
-							
 						}
-					
-					
-				}
+					}
 					dPointer++;
-										
+
 				}
-				
+
 				else {
 					int size = byteRec[dPointer];
 					for(int i = 0; i<size; i++) {
@@ -435,28 +429,134 @@ public class DnsClient {
 					dPointer++;
 					if(byteRec[dPointer]!=0) {
 						alias = alias + ".";
-					
+
 					}
-					
+
+				}
+			//System.out.println(Qname);
+
 			}
+			alias = alias.substring(0, alias.length() - 1);
+
+			System.out.println("NS \t" + alias + "\t" + ttl + "\t" + "auth");
+
+		}
+
+		else if (responseType == "CNAME") {
+			//dPointer++;
+			String alias = "";
+
+			while(byteRec[dPointer]!=0) {
+
+
+				if((byteRec[dPointer] & 0xFF) >= 0xC0) {
+
+					dPointer++;
+					int pointer = byteRec[dPointer];
+					while(byteRec[pointer]!=0) {
+
+						int size = byteRec[pointer];
+						for(int i =0; i<size; i++) {
+							pointer++;
+							alias = alias + (char)byteRec[pointer];
+						}
+
+						dPointer++;
+						if(byteRec[dPointer]!=0) {
+							alias = alias + ".";
+							pointer++;
+
+						}
+
+
+					}
+					dPointer++;
+
+				}
+
+				else {
+					int size = byteRec[dPointer];
+					for(int i = 0; i<size; i++) {
+						dPointer++;
+						alias = alias + (char)byteRec[dPointer];
+					}
+					dPointer++;
+					if(byteRec[dPointer]!=0) {
+						alias = alias + ".";
+
+					}
+
+				}
 			}
 			//System.out.println(alias);
-	        alias = alias.substring(0, alias.length() - 1);
+			alias = alias.substring(0, alias.length() - 1);
 
 			System.out.println("CNAME \t" + alias + "\t" + ttl + "\t" + "auth or not");
 		}
 		else if (responseType == "MX") {
-		
+			
+			
+			int pref = (receiveData[dPointer] << 8) | receiveData[dPointer + 1];
+			dPointer++;
+
+			
+			String alias = "";
+
+			while(byteRec[dPointer]!=0) {
+
+
+				if((byteRec[dPointer] & 0xFF) >= 0xC0) {
+
+					dPointer++;
+					int pointer = byteRec[dPointer];
+					while(byteRec[pointer]!=0) {
+
+						int size = byteRec[pointer];
+						for(int i =0; i<size; i++) {
+							pointer++;
+							alias = alias + (char)byteRec[pointer];
+						}
+
+						dPointer++;
+						if(byteRec[dPointer]!=0) {
+							alias = alias + ".";
+							pointer++;
+						}
+					}
+					dPointer++;
+
+				}
+
+				else {
+					int size = byteRec[dPointer];
+					for(int i = 0; i<size; i++) {
+						dPointer++;
+						alias = alias + (char)byteRec[dPointer];
+					}
+					dPointer++;
+					if(byteRec[dPointer]!=0) {
+						alias = alias + ".";
+
+					}
+
+				}
+			//System.out.println(Qname);
+
+			}
+			//alias = alias.substring(0, alias.length() - 1);
+
+			System.out.println("MX " + alias + "\t" + ttl + "\t" + pref);
+
 		}
-		
+
 		// Close the socket
 		clientSocket.close();
 	}
-	
+
 	private static void maxRetriesError(int x) throws SocketTimeoutException {
 		throw new SocketTimeoutException ();
 	}
-	
+
 	public static int unByte(byte d) {
 		return (d & 0xFF);
 	}
